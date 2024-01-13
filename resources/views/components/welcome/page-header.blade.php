a1ca80bd9a503bd7ea1debfddae2c39b4bf5ee6f
Md Shahidul Islam <contact.shahidul@gmail.com>
2024-01-13 11:13:19 +0600

    update blog design

--- resources/views/components/welcome/page-header.blade.php
+++ resources/views/components/welcome/page-header.blade.php
@@ -6,7 +6,12 @@
                 <h1>@yield('page_title')</h1>
                 <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
                     <ol class="breadcrumb justify-content-center">
-                        <li class="breadcrumb-item"><a href="/" class="text-decoration-none text-orange">Home</a></li>
+                        <li class="breadcrumb-item"><a href="/" class="text-decoration-none @if(request()->is("blog/*")) text-white @else text-orange @endif">Home</a></li>
+                        
+                        @if(request()->is("blog/*"))
+                        <li class="breadcrumb-item"><a href="/blogs" class="text-decoration-none text-orange">Blogs</a></li>
+                        @endif
+                        
                         <li class="breadcrumb-item" aria-current="page">@yield('page_title')</li>
                     </ol>
                 </nav>
